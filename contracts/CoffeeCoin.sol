02ddbbd3871ca44ace3d427b26e9021fc56cb18b
Sergey Kassil <s.kassil@campus.lmu.de>
2023-07-24 13:06:33 +0200

    (feat) add tipping ledger for baristas

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -6,9 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CoffeeCoin is ERC20, Ownable {
     mapping(address => bool) isBarista;
-    //mapping(address => uint256) baristaTipWallet;
+    mapping(address => uint256) baristaTipWallet;
 
-    mapping(address => uint256) orderbook;
+    struct Order {
+        uint256 fullAmountNoDecimals;
+        uint256 tip;
+    }
+
+    mapping(address => Order) orderbook;
 
     uint256 freshBrewPrice = 0.002 ether;
 
@@ -22,17 +27,28 @@ contract CoffeeCoin is ERC20, Ownable {
     }
 
     function brew(address to) public onlyBarista {
-        require(orderbook[to] > 0, "No orders were made by this user!");
+        require(orderbook[to].fullAmountNoDecimals > 0, "No orders were made by this user!");
 
-        _mint(to, numTokens(orderbook[to]));
-        orderbook[to] = 0;
+        uint256 profit = freshBrewPrice * orderbook[to].fullAmountNoDecimals;
+        uint256 tip = orderbook[to].tip;
 
-        payable(owner()).transfer(freshBrewPrice * orderbook[to]);
+        baristaTipWallet[_msgSender()] = tip;
+
+        orderbook[to].fullAmountNoDecimals = 0;
+        orderbook[to].tip = 0;
+
+        _mint(to, numTokens(orderbook[to].fullAmountNoDecimals));
+        payable(owner()).transfer(profit);
     }
 
     function orderCoffee(uint256 fullAmountNoDecimals) external payable {
-        require(msg.value >= freshBrewPrice * fullAmountNoDecimals, "Not enough funds provided for ordering coffee!");
-        orderbook[_msgSender()] = fullAmountNoDecimals;
+        uint256 price = freshBrewPrice * fullAmountNoDecimals;
+        require(msg.value >= price, "Not enough funds provided for ordering coffee!");
+
+        uint256 tip = msg.value - price;
+
+        orderbook[_msgSender()].fullAmountNoDecimals = fullAmountNoDecimals;
+        orderbook[_msgSender()].tip = tip;
     }
 
     function drinkOneCoffee() external {
@@ -40,11 +56,13 @@ contract CoffeeCoin is ERC20, Ownable {
     }
 
     function revokeOrder() external {
-        require(orderbook[_msgSender()] > 0, "You have no running orders!");
+        require(orderbook[_msgSender()].fullAmountNoDecimals > 0, "You have no running orders!");
 
-        orderbook[_msgSender()] = 0;
+        orderbook[_msgSender()].fullAmountNoDecimals = 0;
+        orderbook[_msgSender()].tip = 0;
 
-        payable(_msgSender()).transfer(freshBrewPrice * orderbook[_msgSender()]);
+        payable(_msgSender()).transfer(freshBrewPrice * orderbook[_msgSender()].fullAmountNoDecimals);
+        payable(_msgSender()).transfer(orderbook[_msgSender()].tip);
     }
 
     function employNewBarista(address newBarista) external onlyOwner {
