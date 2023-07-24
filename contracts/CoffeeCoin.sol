ee6846078c6397f852fc8deeb9974d6584897510
Sergey Kassil <s.kassil@campus.lmu.de>
2023-07-24 12:23:50 +0200

    (feat) create orderbook for ordering coffee

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -6,17 +6,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CoffeeCoin is ERC20 {
     mapping(address => bool) isBarista;
 
+    mapping(address => uint256) orderbook;
+
     constructor() ERC20("CoffeeCoin", "COF") {
-        isBarista[msg.sender] = true;
+        isBarista[_msgSender()] = true;
     }
 
     modifier onlyBarista() {
-        require(isBarista[msg.sender], "Only baristas can brew a coffee here!");
+        require(isBarista[_msgSender()], "Only baristas can brew a coffee here!");
         _;
     }
 
     function brew(address to, uint256 fullAmountNoDecimals) public onlyBarista {
         _mint(to, numTokens(fullAmountNoDecimals));
+    }
+
+    function orderCoffee(uint256 fullAmountNoDecimals) external {
+        orderbook[_msgSender()] = fullAmountNoDecimals;
     }
 
     function numTokens(uint256 amount) public view returns (uint256) {
