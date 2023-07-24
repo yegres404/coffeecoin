0a0b28234c8f6e31b3382a3b3258c352f7f052a6
Sergey Kassil <s.kassil@campus.lmu.de>
2023-07-24 12:21:18 +0200

    (feat) brew (mint) functionality for baristas

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoffeeCoin is ERC20 {
-    constructor() ERC20("CoffeeCoin", "COF") {}
+    mapping(address => bool) isBarista;
 
-    function _numTokens(uint256 amount) public view returns (uint256) {
+    constructor() ERC20("CoffeeCoin", "COF") {
+        isBarista[msg.sender] = true;
+    }
+
+    modifier onlyBarista() {
+        require(isBarista[msg.sender], "Only baristas can brew a coffee here!");
+        _;
+    }
+
+    function brew(address to, uint256 fullAmountNoDecimals) public onlyBarista {
+        _mint(to, numTokens(fullAmountNoDecimals));
+    }
+
+    function numTokens(uint256 amount) public view returns (uint256) {
         return amount * (10 ** decimals());
     }
 }
