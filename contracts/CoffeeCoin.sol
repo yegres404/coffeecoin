d6dadef32ee45735aedc45243605af20afd7a976
Sergey Kassil <s.kassil@campus.lmu.de>
2023-07-24 12:07:39 +0200

    (feat) helper function for whole token amount

--- contracts/CoffeeCoin.sol
+++ contracts/CoffeeCoin.sol
@@ -5,4 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoffeeCoin is ERC20 {
     constructor() ERC20("CoffeeCoin", "COF") {}
+
+    function _numTokens(uint256 amount) public view returns (uint256) {
+        return amount * (10 ** decimals());
+    }
 }
